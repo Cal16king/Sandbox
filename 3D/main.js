60f370302f926d79b08675685c253bbd5f47e398
ebeshero <ebbondar@gmail.com>
2024-11-22 15:10:11 -0500

    functioning three js yayayayay

--- 3D/main.js
+++ 3D/main.js
@@ -1,12 +1,11 @@
+import * as THREE from 'three';
 //import './style.css'
 //import javascriptLogo from './javascript.svg'
 //import viteLogo from '/vite.svg'
 //import { setupCounter } from './counter.js'
-import './style.css'
-import * as THREE from 'three';
+
 
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 
@@ -15,12 +14,6 @@ const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
 });
 
-renderer.render(scene, camera);
-
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(50);
-camera.position.setX(-3);
 
 const geometry = new THREE.BoxGeometry(10, 10, 10);
 
@@ -29,11 +22,38 @@ const geometry = new THREE.BoxGeometry(10, 10, 10);
 const material = new THREE.MeshBasicMaterial( { color: 0xFF6347 } );
 
 const cube = new THREE.Mesh( geometry, material );
-
-scene.add( cube );
-
 cube.position.z = -15;
 cube.position.x = -15;
 
 cube.rotation.x = 2;
 cube.rotation.y = .5;
+
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff);
+ambientLight.position.set(25, -15, -400);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(-1, -10, -10);
+
+scene.add( cube );
+scene.add(pointLight);
+scene.add(ambientLight);
+
+//Render the scene:
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(50);
+camera.position.setX(-3);
+camera.position.setY(10);
+renderer.render(scene, camera);
+
+function animate() {
+    requestAnimationFrame( animate );
+    // slowly rotate the cube:
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+}
+
+animate();
