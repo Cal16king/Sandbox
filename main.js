3cf227d4bb8a572c79c229cd136afa9d7a77f47d
Cal16king <cal16king@gmail.com>
2024-12-21 04:24:38 -0500

    Updates

--- main.js
+++ main.js
@@ -23,13 +23,24 @@ const geometry = new THREE.BoxGeometry(10, 10, 10);
 
 const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
 
-const cube = new THREE.Mesh( geometry, material,  );
+//orbit controls
+const control = new OrbitControls(camera, renderer.domElement);
+
+const cubeTexture = new THREE.TextureLoader().load('img/NicolaSamoriNOF.jpg');
+const cube = new THREE.Mesh( new THREE.BoxGeometry(10,10,10), new THREE.MeshBasicMaterial({map: cubeTexture}) );
 cube.position.z = -25;
 cube.position.x = 8;
 
 
 cube.rotation.x = 2;
 cube.rotation.y = .5;
+
+
+const ballTexture = new THREE.TextureLoader().load('img/kirby.jpg');
+const ball = new THREE.Mesh( new THREE.SphereGeometry(5, 32, 32), new THREE.MeshBasicMaterial({map: ballTexture}) );
+ball.position.z = 0;
+ball.position.x = -15;
+
 
 // Lights
 
@@ -39,12 +50,15 @@ pointLight.position.set(0, -10, 10);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 ambientLight.position.set(25, -15, -400);
 
-
+const pointLight2 = new THREE.PointLight(0xffffff, 7);
+pointLight2.position.set(50, 30, -20);
 
 scene.add( cube );
+scene.add( ball );
 scene.add(pointLight);
+scene.add( pointLight2 );
 scene.add(ambientLight);
-
+scene.background = new THREE.TextureLoader().load('img/retroGrid.jpg');
 //Render the scene:
 
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -53,6 +67,8 @@ camera.position.setZ(50);
 camera.position.setX(-3);
 //camera.position.setY(10);
 renderer.render(scene, camera);
+
+
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -67,6 +83,8 @@ function animate() {
     // slowly rotate the cube:
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+    ball.rotation.x += .01;
+    ball.rotation.y += -.02;
 
     renderer.render( scene, camera );
 }
